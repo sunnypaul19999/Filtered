618f8dd87ccd4ec5386400f94873de80c463bede
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-01-08 16:32:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class NumCards extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      range: 0,
+      randomNumberList: [],
+    };
+  }
+
+  shouldComponentUpdate() {
+    return true;
+  }
+
+  componentDidMount() {
+    let generateNumber = document.querySelector(`.generate-number>button`);
+    generateNumber.addEventListener('click', this.generateOnButtonClick.bind(this));
+    let odd = document.querySelector(`.options>button[id="oddbutton"]`);
+    odd.addEventListener('click', this.showFilterOdd.bind(this));
+    let even = document.querySelector(`.options>button[id="evenbutton"]`);
+    even.addEventListener('click', this.showFilterEven.bind(this));
+  }
+
+  generateOnButtonClick() {
+    ReactDOM.render(<div></div>, document.querySelector('.filtered'));
+    let input = document.querySelector(`.number-of-input>input`);
+    let value = input.value;
+    console.log(`entered value: ${value}`);
+    this.setState({
+      range: value,
+      randomNumberList: this.generate(value),
+    });
+  }
+
+
+  generate(range) {
+    let randomNumberList = [];
+    let randomNumber;
+    while (range > 0) {
+      //let digits = console.log(Math.pow(10, range.toString().length));
+      randomNumber = parseInt(Math.random() * 10);
+      randomNumberList.push(randomNumber);
+      range--;
+    }
+
+    return randomNumberList;
+  }
+
+
+  showFilterOdd() {
+    console.log('odd filter');
+    ReactDOM.render(this.createNumCards('odd'), document.querySelector('.filtered'));
+  }
+
+  showFilterEven() {
+    console.log('even filter');
+    ReactDOM.render(this.createNumCards('even'), document.querySelector('.filtered'));
+  }
+
+  numCard(value) {
+    return (
+      <div className="card num-card" >
+        <div className="card-body">
+          <div className="card-title" id={value}>{value}</div>
+        </div>
+      </div>
+    );
+  }
+
+
+  createNumCards(filter) {
+
+    if (this.state.randomNumberList.length > 0) {
+      let numCards = [];
+
+      if (filter !== undefined) {
+        numCards.push(
+          (
+            <div style={{ width: '100%', 'text-align': 'center' }}>
+              <h1>
+                <b>Filtered Array</b>
+              </h1>
+            </div>
+          )
+        );
+      }
+
+      this.state.randomNumberList.forEach((value) => {
+        if (filter === 'odd') {
+          if (value % 2 !== 0) {
+            numCards.push(this.numCard(value));
+          }
+        } else if (filter === 'even') {
+          if (value % 2 === 0) {
+            numCards.push(this.numCard(value));
+          }
+        } else {
+          numCards.push(this.numCard(value));
+        }
+      });
+      return numCards;
+    } else {
+      return [];
+    }
+  }
+
+  render() {
+    return (
+      <div className="num-cards" id="originalList">
+        {this.createNumCards()}
+      </div>
+    );
+  }
+}
+
+
+class Filter extends React.Component {
+
+  render() {
+    return (
+      <div className="container" id="root">
+
+        <div className="number-of-input">
+          <input data-testid="numberOfInput" className="form-control form-control-lg" type="text"
+            placeholder="Enter the numbers of inputs" />
+        </div>
+
+        <div className="generate-number">
+          <button data-testid="generateNumber" type="button" className="btn btn-success btn-lg btn-block">
+            Generate Random Number</button>
+        </div>
+
+        <NumCards />
+
+        <div className="options">
+          <button id="oddbutton" type="button" className="btn btn-success">ODD</button>
+
+          <button id="evenbutton" type="button" className="btn btn-success">EVEN</button>
+        </div>
+
+        <div className="filtered num-cards" id="filteredList"></div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Filter />, document.getElementById('root'));
